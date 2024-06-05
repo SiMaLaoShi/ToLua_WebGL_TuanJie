d66d90c0030c83cc7d5ec2752678ce940d5bd7ea
simalaoshi <1742495214@qq.com>
2024-06-05 09:41:35 +0800

    1，初始化版本库相关

---
+++ Assets/ToLua/Examples/01_HelloWorld/HelloWorld.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using LuaInterface;
+using System;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour
+{
+    IEnumerator Init2()
+    {
+        yield return AssetBundleLoad.Init();
+        Init();
+    }
+
+    void Init()
+    {
+
+        LuaState lua = new LuaState();
+        // lua.AddSearchPath(Application.streamingAssetsPath + "/ToLua/Lua/?");
+        // lua.AddSearchPath(Application.streamingAssetsPath + "/ToLua/Lua/?.lua");
+        lua.Start();
+        string hello =
+            @"                
+                print('hello tolua#')                                  
+            ";
+
+        lua.DoString(hello, "HelloWorld.cs");
+        lua.CheckTop();
+        lua.Dispose();
+        lua = null;
+    }
+
+    void Awake()
+    {
+        StartCoroutine(Init2());
+        // Init();
+        // WeChatWASM.WX.OpenProfileStats();
+
+
+    }
+}
